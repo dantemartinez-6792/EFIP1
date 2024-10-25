6174c9134b642d6fc9fc4ab64226bde1d7772699
dantemartinez-6792 <dante_martinez6792@hotmail.com>
2024-10-25 13:52:16 -0300

    Borrado de datos de tablas.sql

--- Borrado de datos de tablas.sql
+++ Borrado de datos de tablas.sql
@@ -2,11 +2,9 @@ use efip1;
 delete from operador;
 delete from equipo;
 delete from inicioEntrenamiento;
-delete from entrenamientoOperador;
 delete from registroHoras;
 
 select*from operador;
 select*from equipo;
 select*from inicioEntrenamiento;
-select*from entrenamientoOperador;
 select*from registroHoras;
